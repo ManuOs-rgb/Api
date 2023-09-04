01c35b7e7be04de373d2904be764fec763c50147
Héctor Vázquez <manuelbv@gmail.com>
2023-09-04 11:45:19 -0600

    first commit

---
+++ src/main/java/med/voll/api/domain/paciente/DatosRegistroPaciente.java
@@ -0,0 +1,14 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import med.voll.api.domain.direccion.DatosDireccion;
+
+public record DatosRegistroPaciente(
+        @NotBlank String nombre,
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 0, max = 15) String telefono,
+        @NotBlank String documento,
+       @NotNull @Valid DatosDireccion direccion
+) {
+}
---
+++ src/main/java/med/voll/api/infra/errores/ManejadorDeErorres.java
@@ -0,0 +1,31 @@
+package med.voll.api.infra.errores;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ManejadorDeErorres {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity manejarErrores404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity manejarError400(MethodArgumentNotValidException ex){
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toString());
+    }
+
+    private record DatosErrorValidacion(String campo, String mensaje){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
